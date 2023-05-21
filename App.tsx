755864c075b3557e0aaf9303e7e9b1935cd3afa2
Kudo24 <alistaire024@gmail.com>
2023-05-21 16:52:41 +0800

    Add: Navigator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+function Dashboard() {
+  return (
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  );
+}
+
+function Settings() {
+  return (
+    <View>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+function Tab() {
+  return (
+    <BottomTab.Navigator>
+      <BottomTab.Screen name="Dashboard" component={Dashboard} />
+      <BottomTab.Screen name="Settings" component={Settings} />
+    </BottomTab.Navigator>
+  );
+}
+
+function Login({ navigation }: any) {
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+      <Button title="next" onPress={() => navigation.navigate("Signup")} />
+    </View>
+  );
+}
+
+function Signup() {
+  return (
+    <View style={styles.container}>
+      <Text>Signup</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Tab" component={Tab} />
+        <Stack.Screen name="login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
