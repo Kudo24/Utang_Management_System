070dbde672e9f3bc8165a84984ea812c9abb7aa3
Kudo24 <alistaire024@gmail.com>
2023-05-21 17:29:41 +0800

    Add: Homepage

--- navigator/RootNavigator.tsx
+++ navigator/RootNavigator.tsx
@@ -3,13 +3,18 @@ import React from "react";
 
 // Tab Navigator
 import TabNavigator from "./TabNavigator";
+import Home from "../screens/Home";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const RootStack = createNativeStackNavigator();
 const RootNavigator = () => {
   return (
-    <RootStack.Navigator screenOptions={{ headerShown: false }}>
+    <RootStack.Navigator
+      screenOptions={{ headerShown: false }}
+      initialRouteName="Home"
+    >
       <RootStack.Screen name="Main" component={TabNavigator} />
+      <RootStack.Screen name="Home" component={Home} />
     </RootStack.Navigator>
   );
 };
--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -5,13 +5,38 @@ import React from "react";
 import Mine from "../screens/Mine";
 import Others from "../screens/Others";
 
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === "Mine") {
+            return (
+              <Ionicons
+                name="person"
+                size={24}
+                color={focused ? "#59C1CC" : "gray"}
+              />
+            );
+          } else if (route.name === "Others") {
+            return (
+              <FontAwesome5
+                name="people-arrows"
+                size={24}
+                color={focused ? "#EB6A7C" : "gray"}
+              />
+            );
+          }
+        },
+      })}
+    >
       <Tab.Screen name="Mine" component={Mine} />
       <Tab.Screen name="Others" component={Others} />
     </Tab.Navigator>
